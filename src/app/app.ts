b672bdbcd867f52b788f314ca04a29a903a85560
Vansh Singh <vanshsin784@gmail.com>
2026-04-10 15:03:54 +0530

    feat: initialise repository

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,14 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('dev-blog-FE');
+  protected readonly author = signal('Vansh Singh');
+
+  onClickingOfButton() {
+    if (this.author() === 'Vansh Singh') return this.author.set('Hello World');
+    return this.author.set('Vansh Singh');
+  }
 }
